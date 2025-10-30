e3a6937848a3109e9ea6b9ff139cf35867f58d50
Joseph Holten <joseph@holten.com>
2025-10-30 15:11:23 +0100

    update fullscreen
    Fix:
      - get actual display size of monitor where mouse is
    
    Support:
      - runtime bg color
      - runtime font
      - runtime text color
      - runtime message

--- batsignal/fullscreen_warning.c
+++ batsignal/fullscreen_warning.c
@@ -2,21 +2,86 @@
 #include <X11/Xutil.h>
 #include <X11/Xatom.h>
 #include <X11/Xft/Xft.h>  // Add Xft headers
+#include <X11/extensions/Xrandr.h> // for multi monitor info
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-const char* message = "BATTERY CRITICALLY LOW";
+typedef struct {
+  int width;
+  int height;
+} MonitorSize;
 
-int main() {
+void get_monitor_size_where_mouse(Display *display, int screen, MonitorSize *monitor_size) {
+    Window root = RootWindow(display, screen);
+
+    Window root_return, child_return;
+    int root_x, root_y, win_x, win_y;
+    unsigned int mask_return;
+    XQueryPointer(display, root, &root_return, &child_return,
+                  &root_x, &root_y, &win_x, &win_y, &mask_return);
+
+    XRRScreenResources *resources = XRRGetScreenResources(display, root);
+    int done = 0;
+
+    for (int i = 0; i < resources->noutput && !done; i++) {
+        XRROutputInfo *output = XRRGetOutputInfo(display, resources, resources->outputs[i]);
+        if (output->connection == RR_Connected && output->crtc) {
+            XRRCrtcInfo *crtc = XRRGetCrtcInfo(display, resources, output->crtc);
+
+            if (root_x >= crtc->x && root_x < crtc->x + (int)crtc->width && root_y >= crtc->y && root_y < crtc->y + (int)crtc->height) {
+              monitor_size->width = crtc->width;
+              monitor_size->height = crtc->height;
+              done = 1;
+            }
+
+            XRRFreeCrtcInfo(crtc);
+        }
+        XRRFreeOutputInfo(output);
+    }
+    XRRFreeScreenResources(resources);
+}
+
+
+int main(int argc, char** argv) {
     Display *display;
     Window window;
     XEvent event;
     int screen;
     GC gc;
 
-    /* Open display */
+    const char* message = "BATTERY CRITICALLY LOW";
+    const char* USAGE = "usage: %s [-m message] [-f font] [-b bg_color] [-t text_color] [-h]\n";
+    const char* font = "Liberation Mono";
+    const char* color_str = "red";
+    const char* tcolor_str = "white";
+
+    int opt;
+    while ((opt = getopt(argc, argv, "m:f:hb:t:")) != -1) {
+      switch (opt) {
+        case 'm':
+          message = optarg;
+          break;
+        case 'f':
+          font = optarg;
+          break;
+        case 'h':
+          printf(USAGE, argv[0]);
+          return 0;
+        case 'b':
+          color_str = optarg;
+          break;
+        case 't':
+          tcolor_str = optarg;
+          break;
+        default:
+          fprintf(stderr, "warning: ignoring unrecognized option '%c'", opt);
+          fprintf(stderr, USAGE, argv[0]);
+          break;
+      }
+    }
+
     display = XOpenDisplay(NULL);
     if (display == NULL) {
         fprintf(stderr, "ERROR: fullscreen: Cannot open display\n");
@@ -25,17 +90,31 @@ int main() {
 
     screen = DefaultScreen(display);
 
-    /* Get screen dimensions */
-    int screen_width = DisplayWidth(display, screen);
-    int screen_height = DisplayHeight(display, screen);
+    MonitorSize monitor;
+    get_monitor_size_where_mouse(display, screen, &monitor);
 
-    /* Create window at full screen size */
+    Colormap colormap = DefaultColormap(display, screen);
+
+    XColor color, tcolo;
+    if (!XParseColor(display, colormap, color_str, &color)) {
+      fprintf(stderr, "ERROR: failed to parse color: %s\n", color_str);
+      XCloseDisplay(display);
+      return 1;
+    }
+
+    if (!XAllocColor(display, colormap, &color)) {
+      fprintf(stderr, "ERROR: failed to allocate color: %s\n", color_str);
+      return 1;
+    }
+
+    unsigned long border_width = 0, border = BlackPixel(display, screen),
+      background = color.pixel;
+    int win_x = 0, win_y = 0;
+
     window = XCreateSimpleWindow(display, RootWindow(display, screen),
-                                0, 0, screen_width, screen_height, 0,
-                                BlackPixel(display, screen), 
-                                0xFF0000); /* Red background */
+      win_x, win_y, monitor.width, monitor.height, border_width, border,
+      background);
 
-    /* Set window name and class */
     XStoreName(display, window, "Critical Warning");
 
     XClassHint class_hint;
@@ -43,7 +122,6 @@ int main() {
     class_hint.res_class = "CriticalWarning";
     XSetClassHint(display, window, &class_hint);
 
-    /* Make the window fullscreen */
     Atom wm_state = XInternAtom(display, "_NET_WM_STATE", False);
     Atom fullscreen = XInternAtom(display, "_NET_WM_STATE_FULLSCREEN", False);
 
@@ -58,78 +136,56 @@ int main() {
     fullscreen_event.xclient.data.l[1] = fullscreen;
     fullscreen_event.xclient.data.l[2] = 0;
 
-    /* Select inputs */
     XSelectInput(display, window, ExposureMask | KeyPressMask);
 
-    /* Create GC for basic X11 operations */
     gc = XCreateGC(display, window, 0, 0);
 
-    /* Initialize Xft drawing */
     Visual *visual = DefaultVisual(display, screen);
-    Colormap colormap = DefaultColormap(display, screen);
 
-    /* Create Xft font */
-    XftFont *xft_font = XftFontOpen(display, screen,
-                                   XFT_FAMILY, XftTypeString, "Liberation Mono",
-                                   XFT_WEIGHT, XftTypeInteger, XFT_WEIGHT_BOLD,
-                                   XFT_SIZE, XftTypeDouble, 64.0,
-                                   NULL);
+    XftFont* xft_font = XftFontOpen(display, screen,
+      XFT_FAMILY, XftTypeString, font,
+      XFT_WEIGHT, XftTypeInteger, XFT_WEIGHT_BOLD,
+      XFT_SIZE, XftTypeDouble, 64.0, NULL);
 
     if (!xft_font) {
-        fprintf(stderr, "ERROR: fullscreen: Cannot load Liberation Mono font, trying fallback\n");
-        xft_font = XftFontOpen(display, screen,
-                              XFT_FAMILY, XftTypeString, "monospace",
-                              XFT_WEIGHT, XftTypeInteger, XFT_WEIGHT_BOLD,
-                              XFT_SIZE, XftTypeDouble, 64.0,
-                              NULL);
+      printf("ERROR: fullscreen: couldn't load font '%s' or any other fallback", font);
+
+      XFreeGC(display, gc);
+      XDestroyWindow(display, window);
+      XCloseDisplay(display);
+
+      return 1;
     }
 
-    /* Create Xft colors */
     XftColor xft_color;
-    XRenderColor render_color;
-    render_color.red = 0xFFFF;    /* White text (full red component) */
-    render_color.green = 0xFFFF;  /* White text (full green component) */
-    render_color.blue = 0xFFFF;   /* White text (full blue component) */
-    render_color.alpha = 0xFFFF;  /* Fully opaque */
-
-    XftColorAllocValue(display, visual, colormap, &render_color, &xft_color);
-
-    /* Create Xft draw context */
+    XftColorAllocName(display, visual, colormap, tcolor_str, &xft_color);
     XftDraw *xft_draw = XftDrawCreate(display, window, visual, colormap);
-
-    /* Map window */
     XMapRaised(display, window);
-
-    /* Send fullscreen event */
     XSendEvent(display, RootWindow(display, screen), False,
                SubstructureNotifyMask | SubstructureRedirectMask,
                &fullscreen_event);
 
     XFlush(display);
 
-    /* Event loop */
     while (1) {
         XNextEvent(display, &event);
 
         if (event.type == Expose) {
             if (xft_font) {
-                /* Calculate text size for centering */
                 XGlyphInfo extents;
                 XftTextExtents8(display, xft_font, (XftChar8 *)message, strlen(message), &extents);
 
-                int x = (screen_width - extents.width) / 2;
-                int y = (screen_height + extents.height) / 2;
+                int x = (monitor.width - extents.width) / 2;
+                int y = (monitor.height + extents.height) / 2;
 
-                /* Draw text with Xft for high-quality rendering */
                 XftDrawString8(xft_draw, &xft_color, xft_font, x, y,
                                (XftChar8 *)message, strlen(message));
             }
         } else if (event.type == KeyPress) {
-            break; /* Exit on any keypress */
+            break;
         }
     }
 
-    /* Clean up */
     if (xft_font) XftFontClose(display, xft_font);
     XftDrawDestroy(xft_draw);
     XftColorFree(display, visual, colormap, &xft_color);
