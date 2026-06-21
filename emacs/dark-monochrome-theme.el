496f0c8f3449c05641b6757c47599b8c48cd7a08
Joseph Holten <joseph@holten.com>
2026-06-21 12:04:32 -0400

    update colorpalette

--- emacs/dark-monochrome-theme.el
+++ emacs/dark-monochrome-theme.el
@@ -73,6 +73,25 @@
    ((t (:foreground "#df2c14"))))
  '(doom-modeline-warning
    ((t (:foreground "#dc6601"))))
+
+ ;; vterm ANSI palette -- high-contrast, readable on black, kept in sync
+ ;; with st (see st/config.h). Colour is taken from each face's :foreground.
+ '(vterm-color-black          ((t (:foreground "#000000"))))
+ '(vterm-color-red            ((t (:foreground "#ff5f5f"))))
+ '(vterm-color-green          ((t (:foreground "#5fd75f"))))
+ '(vterm-color-yellow         ((t (:foreground "#ffd75f"))))
+ '(vterm-color-blue           ((t (:foreground "#5f9fff"))))
+ '(vterm-color-magenta        ((t (:foreground "#ff5fd7"))))
+ '(vterm-color-cyan           ((t (:foreground "#5fd7d7"))))
+ '(vterm-color-white          ((t (:foreground "#d0d0d0"))))
+ '(vterm-color-bright-black   ((t (:foreground "#6c6c6c"))))
+ '(vterm-color-bright-red     ((t (:foreground "#ff8787"))))
+ '(vterm-color-bright-green   ((t (:foreground "#87ff87"))))
+ '(vterm-color-bright-yellow  ((t (:foreground "#ffff87"))))
+ '(vterm-color-bright-blue    ((t (:foreground "#87bfff"))))
+ '(vterm-color-bright-magenta ((t (:foreground "#ff87ff"))))
+ '(vterm-color-bright-cyan    ((t (:foreground "#87ffff"))))
+ '(vterm-color-bright-white   ((t (:foreground "#ffffff"))))
 )
 
 (provide-theme 'dark-monochrome)
